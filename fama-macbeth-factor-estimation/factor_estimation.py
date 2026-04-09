5e593d8b61a1025d4bcce310ccaff3185c46234f
MichaelKA-J <98190608+MichaelKA-J@users.noreply.github.com>
2026-04-09 17:24:56 -0400

    Update factor_estimation.py

--- fama-macbeth-factor-estimation/factor_estimation.py
+++ fama-macbeth-factor-estimation/factor_estimation.py
@@ -1,18 +1,11 @@
 import statsmodels.api as sm; import numpy as np; import pandas as pd
+from sklearn.linear_model import LinearRegression as lr
 
-df = pd.read_excel('Fama_MacBeth.xlsx', index_col = 'Date')
+df = pd.read_excel('Fama_MacBeth.xlsx')
+excess_df = df.iloc[:, 1:26].sub(df.RF, axis = 0)
 
-rf_df = pd.concat([df.RF] * 25, axis = 1) 
-excess_df = df.iloc[:, 0:25] - rf_df.iloc[:, 0:25].values 
-X = sm.add_constant(df[['Mkt-RF', 'CPIAUCSL_PCH', 'PCE_PCH']]) 
+# 1st stage
+betas = sm.add_constant(lr().fit(df.iloc[:, [26, 28, 29]], excess_df).coef_)
 
-betas_df = pd.DataFrame()
-for i in range(25):
-    mod_first_stage = sm.OLS(excess_df.iloc[:, i], X).fit()
-    betas_df[i + 1] = mod_first_stage.params.iloc[1:4] 
-betas_df = sm.add_constant(betas_df.T)
-
-r_bar = np.mean(df.iloc[:, 0:25], axis = 0) 
-
-mod_second_stage = sm.OLS(r_bar.values, betas_df).fit()
-print(mod_second_stage.summary())
+# 2nd stage
+sm.OLS(np.mean(df.iloc[:, 1:26], axis = 0).values, betas).fit().summary()
